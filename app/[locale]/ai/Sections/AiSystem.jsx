7b5b95bcb62889fa537e8c97cdfb7b60ac7fdedd
sebastian <sebastian.hallquist@gmail.com>
2024-12-17 18:44:30 +0100

    changes

--- app/[locale]/ai/Sections/AiSystem.jsx
+++ app/[locale]/ai/Sections/AiSystem.jsx
@@ -8,19 +8,13 @@ const AiSystem = () => {
     <div className={styles.container}>
       <p>{t("AiSystem1")}</p>
       <br />
-      <ul className="list-disc">
+      <ul className="list-disc flex flex-col gap-y-2">
         <li>{t("AiList1")}</li>
-        <br />
         <li>{t("AiList2")}</li>
-        <br />
         <li>{t("AiList3")}</li>
-        <br />
         <li>{t("AiList4")}</li>
-        <br />
         <li>{t("AiList5")}</li>
-        <br />
         <li>{t("AiList6")}</li>
-        <br />
       </ul>
     </div>
   );
