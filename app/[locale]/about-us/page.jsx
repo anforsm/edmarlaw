8f3a48bd6999b1d9e17d665547d2dcc11f59c48b
sebastian <sebastian.hallquist@gmail.com>
2024-12-18 02:37:09 +0100

    changes

--- app/[locale]/about-us/page.jsx
+++ app/[locale]/about-us/page.jsx
@@ -87,6 +87,11 @@ const Page = () => {
                   year={"1996"}
                 />
               </div>
+
+              {/* internet sociala medier
+onlineutbildining 2015
+dommännamn köp på eng
+immatrielrättens */}
             </div>
             <div className="lg:pt-36 pt-14">
               <h4 className="text-[32px] font-bold">Publikationer</h4>
@@ -96,11 +101,11 @@ const Page = () => {
                     link={true}
                     company={tNew("Författare till boken")}
                     year={"Norstedts Juridik, 7 uppl., 2021"}
-                    linkTitle={"Domain Names – Strategies and Legal Aspects"}
+                    linkTitle={"Internetpublicering och sociala medier"}
                     linkHref={
-                      "https://www.sweetandmaxwell.co.uk/Product/Dispute-Resolution/Domain-Names-Strategies-and-Legal-Aspects/Hardback-and-eBook-ProView/42960040"
+                      "https://shop.nj.se/products/internetpublicering-och-sociala-medier-1"
                     }
-                    swedishLink={"https://shop.nj.se/products/domannamn"}
+                    // swedishLink={"https://shop.nj.se/products/domannamn"}
                   />
                   <div className="lg:flex gap-x-2 underline hidden font-semibold">
                     <Link
@@ -129,73 +134,18 @@ const Page = () => {
                     link={true}
                     company={"Edmar, Malin;"}
                     year={"Norstedts Juridik, 2015"}
-                    linkTitle={"Domain Names – Strategies and Legal Aspects"}
+                    linkTitle={t("Online education on Internet publishing")}
                     linkHref={
-                      "https://www.sweetandmaxwell.co.uk/Product/Dispute-Resolution/Domain-Names-Strategies-and-Legal-Aspects/Hardback-and-eBook-ProView/42960040"
-                    }
-                  />
-                  <div className="lg:flex lg:flex-col gap-y-1 underline hidden font-semibold">
-                    <div className="lg:flex gap-x-2 underline hidden font-semibold">
-                      <Link
-                        href={
-                          "https://www.sweetandmaxwell.co.uk/Product/Dispute-Resolution/Domain-Names-Strategies-and-Legal-Aspects/Hardback-and-eBook-ProView/42960040"
-                        }
-                      >
-                        Domain Names – Strategies and Legal Aspects
-                      </Link>
-                      <svg
-                        width="18"
-                        height="19"
-                        viewBox="0 0 18 19"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M10.6875 1.59375C10.6875 1.13672 11.0391 0.75 11.5312 0.75H17.1211C17.6133 0.75 17.9648 1.13672 17.9648 1.59375V7.21875C17.9648 7.71094 17.6133 8.0625 17.1211 8.0625C16.6641 8.0625 16.2773 7.71094 16.2773 7.21875V3.63281L8.4375 11.4727C8.12109 11.8242 7.59375 11.8242 7.27734 11.4727C6.92578 11.1562 6.92578 10.6289 7.27734 10.2773L15.1172 2.4375H11.5312C11.0391 2.4375 10.6875 2.08594 10.6875 1.59375ZM2.53125 1.875H7.03125C7.48828 1.875 7.875 2.26172 7.875 2.71875C7.875 3.21094 7.48828 3.5625 7.03125 3.5625H2.53125C2.03906 3.5625 1.6875 3.94922 1.6875 4.40625V16.2188C1.6875 16.7109 2.03906 17.0625 2.53125 17.0625H14.3438C14.8008 17.0625 15.1875 16.7109 15.1875 16.2188V11.7188C15.1875 11.2617 15.5391 10.875 16.0312 10.875C16.4883 10.875 16.875 11.2617 16.875 11.7188V16.2188C16.875 17.625 15.7148 18.75 14.3438 18.75H2.53125C1.125 18.75 0 17.625 0 16.2188V4.40625C0 3.03516 1.125 1.875 2.53125 1.875Z"
-                          fill="#1D1D1D"
-                        />
-                      </svg>
-                    </div>
-                    <div className="lg:flex gap-x-2 underline hidden font-semibold">
-                      <Link
-                        href={
-                          "https://www.sweetandmaxwell.co.uk/Product/Dispute-Resolution/Domain-Names-Strategies-and-Legal-Aspects/Hardback-and-eBook-ProView/42960040"
-                        }
-                      >
-                        Buy it in Swedish here
-                      </Link>
-                      <svg
-                        width="18"
-                        height="19"
-                        viewBox="0 0 18 19"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M10.6875 1.59375C10.6875 1.13672 11.0391 0.75 11.5312 0.75H17.1211C17.6133 0.75 17.9648 1.13672 17.9648 1.59375V7.21875C17.9648 7.71094 17.6133 8.0625 17.1211 8.0625C16.6641 8.0625 16.2773 7.71094 16.2773 7.21875V3.63281L8.4375 11.4727C8.12109 11.8242 7.59375 11.8242 7.27734 11.4727C6.92578 11.1562 6.92578 10.6289 7.27734 10.2773L15.1172 2.4375H11.5312C11.0391 2.4375 10.6875 2.08594 10.6875 1.59375ZM2.53125 1.875H7.03125C7.48828 1.875 7.875 2.26172 7.875 2.71875C7.875 3.21094 7.48828 3.5625 7.03125 3.5625H2.53125C2.03906 3.5625 1.6875 3.94922 1.6875 4.40625V16.2188C1.6875 16.7109 2.03906 17.0625 2.53125 17.0625H14.3438C14.8008 17.0625 15.1875 16.7109 15.1875 16.2188V11.7188C15.1875 11.2617 15.5391 10.875 16.0312 10.875C16.4883 10.875 16.875 11.2617 16.875 11.7188V16.2188C16.875 17.625 15.7148 18.75 14.3438 18.75H2.53125C1.125 18.75 0 17.625 0 16.2188V4.40625C0 3.03516 1.125 1.875 2.53125 1.875Z"
-                          fill="#1D1D1D"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-col">
-                  <TableRow
-                    link={true}
-                    company={tNew("Medförfattare till boken")}
-                    year={"Norstedts Juridik, 2014"}
-                    linkTitle={"I immaterialrättens gränsland"}
-                    linkHref={
-                      "https://www.jure.se/ns/default.asp?url=visatitel.asp?tuid=17706"
+                      "https://www.nj.se/ekurs-internetpublicering-och-sociala-medier"
                     }
                   />
                   <div className="lg:flex gap-x-2 underline hidden font-semibold">
                     <Link
                       href={
-                        "https://www.jure.se/ns/default.asp?url=visatitel.asp?tuid=17706"
+                        "https://www.nj.se/ekurs-internetpublicering-och-sociala-medier"
                       }
                     >
-                      I immaterialrättens gränsland
+                      {t("Online education on Internet publishing")}
                     </Link>
                     <svg
                       width="18"
@@ -214,16 +164,69 @@ const Page = () => {
                 <div className="flex flex-col">
                   <TableRow
                     link={true}
+                    company={tNew("Medförfattare till boken")}
+                    year={"Norstedts Juridik, 2014"}
+                    linkTitle={t("Domain Names – Strategies and Legal Aspects")}
+                    linkHref={"https://shop.nj.se/products/domannamn"}
+                  />
+                  <div className="lg:flex lg:flex-col gap-y-1 underline hidden font-semibold">
+                    <div className="lg:flex gap-x-2 underline hidden font-semibold">
+                      <Link href={"https://shop.nj.se/products/domannamn"}>
+                        {t("Domain Names – Strategies and Legal Aspects")}
+                      </Link>
+                      <svg
+                        width="18"
+                        height="19"
+                        viewBox="0 0 18 19"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M10.6875 1.59375C10.6875 1.13672 11.0391 0.75 11.5312 0.75H17.1211C17.6133 0.75 17.9648 1.13672 17.9648 1.59375V7.21875C17.9648 7.71094 17.6133 8.0625 17.1211 8.0625C16.6641 8.0625 16.2773 7.71094 16.2773 7.21875V3.63281L8.4375 11.4727C8.12109 11.8242 7.59375 11.8242 7.27734 11.4727C6.92578 11.1562 6.92578 10.6289 7.27734 10.2773L15.1172 2.4375H11.5312C11.0391 2.4375 10.6875 2.08594 10.6875 1.59375ZM2.53125 1.875H7.03125C7.48828 1.875 7.875 2.26172 7.875 2.71875C7.875 3.21094 7.48828 3.5625 7.03125 3.5625H2.53125C2.03906 3.5625 1.6875 3.94922 1.6875 4.40625V16.2188C1.6875 16.7109 2.03906 17.0625 2.53125 17.0625H14.3438C14.8008 17.0625 15.1875 16.7109 15.1875 16.2188V11.7188C15.1875 11.2617 15.5391 10.875 16.0312 10.875C16.4883 10.875 16.875 11.2617 16.875 11.7188V16.2188C16.875 17.625 15.7148 18.75 14.3438 18.75H2.53125C1.125 18.75 0 17.625 0 16.2188V4.40625C0 3.03516 1.125 1.875 2.53125 1.875Z"
+                          fill="#1D1D1D"
+                        />
+                      </svg>
+                    </div>
+                    <div className="lg:flex gap-x-2 underline hidden font-semibold">
+                      <Link
+                        href={
+                          "https://www.sweetandmaxwell.co.uk/Product/Dispute-Resolution/Domain-Names-Strategies-and-Legal-Aspects/Hardback-and-eBook-ProView/42960040"
+                        }
+                      >
+                        {t("Also available in English")}
+                      </Link>
+                      <svg
+                        width="18"
+                        height="19"
+                        viewBox="0 0 18 19"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M10.6875 1.59375C10.6875 1.13672 11.0391 0.75 11.5312 0.75H17.1211C17.6133 0.75 17.9648 1.13672 17.9648 1.59375V7.21875C17.9648 7.71094 17.6133 8.0625 17.1211 8.0625C16.6641 8.0625 16.2773 7.71094 16.2773 7.21875V3.63281L8.4375 11.4727C8.12109 11.8242 7.59375 11.8242 7.27734 11.4727C6.92578 11.1562 6.92578 10.6289 7.27734 10.2773L15.1172 2.4375H11.5312C11.0391 2.4375 10.6875 2.08594 10.6875 1.59375ZM2.53125 1.875H7.03125C7.48828 1.875 7.875 2.26172 7.875 2.71875C7.875 3.21094 7.48828 3.5625 7.03125 3.5625H2.53125C2.03906 3.5625 1.6875 3.94922 1.6875 4.40625V16.2188C1.6875 16.7109 2.03906 17.0625 2.53125 17.0625H14.3438C14.8008 17.0625 15.1875 16.7109 15.1875 16.2188V11.7188C15.1875 11.2617 15.5391 10.875 16.0312 10.875C16.4883 10.875 16.875 11.2617 16.875 11.7188V16.2188C16.875 17.625 15.7148 18.75 14.3438 18.75H2.53125C1.125 18.75 0 17.625 0 16.2188V4.40625C0 3.03516 1.125 1.875 2.53125 1.875Z"
+                          fill="#1D1D1D"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <TableRow
+                    link={true}
                     company={tNew("Redaktör och medförfattare till boken")}
                     year={"Jure, 2012"}
-                    linkTitle={"Internetpublicering och sociala medier"}
+                    linkTitle={t("The Twilight Zone of Intellectual Property,")}
                     linkHref={
                       "https://www.jure.se/ns/default.asp?url=visatitel.asp?tuid=17706"
                     }
                   />
                   <div className="lg:flex gap-x-2 underline hidden font-semibold">
-                    <Link href={""}>
-                      {tNew("Internetpublicering och sociala medier")}
+                    <Link
+                      href={
+                        "https://www.jure.se/ns/default.asp?url=visatitel.asp?tuid=17706"
+                      }
+                    >
+                      {t("The Twilight Zone of Intellectual Property,")}
                     </Link>
                     <svg
                       width="18"
