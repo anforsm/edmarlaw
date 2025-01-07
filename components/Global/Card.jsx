326001e6095d3102991e21eb645866d3ee174ab1
Sebastian Hallqvist <sebastian.hallquist@gmail.com>
2025-01-07 11:46:48 +0100

    changes

--- components/Global/Card.jsx
+++ components/Global/Card.jsx
@@ -14,12 +14,7 @@ export default function Card({ title, description, icon, size, link }) {
       data-size={size}
       className="flex data-[size=sm]:flex-row data-[size=sm]:gap-x-4 data-[size=sm]:items-center flex-col gap-y-6 border border-[#CBCDD0] bg-white rounded lg:col-span-4 col-span-12 p-4"
     >
-      <div
-      // style={{
-      //   width: SIZE_MAP[size || "default"].width,
-      //   height: SIZE_MAP[size || "default"].height,
-      // }}
-      >
+      <div>
         {/* <Image src={icon} alt={title} width={36} height={36} /> */}
         {icon}
       </div>
--- components/Global/Footer.jsx
+++ components/Global/Footer.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
-import React from "react";
 import { FaLinkedinIn } from "react-icons/fa";
 
 const Footer = () => {
