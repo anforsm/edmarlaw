3e5dd5da0e28d19f35c9e9bace56f4b1152fe8a3
Sebastian Hallqvist <sebastian.hallquist@gmail.com>
2025-01-07 11:46:53 +0100

    changes

--- components/Global/Card.jsx
+++ components/Global/Card.jsx
@@ -14,10 +14,7 @@ export default function Card({ title, description, icon, size, link }) {
       data-size={size}
       className="flex data-[size=sm]:flex-row data-[size=sm]:gap-x-4 data-[size=sm]:items-center flex-col gap-y-6 border border-[#CBCDD0] bg-white rounded lg:col-span-4 col-span-12 p-4"
     >
-      <div>
-        {/* <Image src={icon} alt={title} width={36} height={36} /> */}
-        {icon}
-      </div>
+      <div>{icon}</div>
       <div
         data-size={size}
         className="flex flex-col gap-y-2 h-full data-[size=sm]:justify-center justify-between"
