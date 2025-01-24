1faf94fad0eb3651ea153d1d0a27dfcfbad7c11a
Sebastian Hallqvist <sebastian.hallqvist@weahead.se>
2025-01-24 15:43:22 +0100

    changes

--- app/[locale]/about-us/publications/page.js
+++ app/[locale]/about-us/publications/page.js
@@ -1,9 +1,4 @@
 "use client";
-import About from "@/components/About/About";
-import Associations from "@/components/About/Associations";
-import MalinEdmar from "@/components/About/MalinEdmar";
-import Hero from "@/components/Global/Hero";
-import AreaOFExpertise from "@/components/Home/AreaOFExpertise";
 import Publications from "@/components/publications/Publications";
 import React from "react";
 
